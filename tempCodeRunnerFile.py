3c024f6dace94b26f7c7dce151b1497b91b06eed
RainbOwKid30 <hauem471@gmail.com>
2023-09-04 19:22:26 +0700

    BT 2

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,10 @@
-so1 = int(input("Moi ban nhap so nguyen thu 1: "))
-so2 = int(input("Moi ban nhap so nguyen thu 2: "))
-SumNguyen = so1 + so2
+so1 = input("Moi ban nhap so nguyen thu 1: ")
+so2 = input("Moi ban nhap so nguyen thu 2: ")
 
-print("Tong hai so nguyen la: ",SumNguyen)
+try:
+    thu1 = int(so1)
+    thu2 = int(so2)
+    tong = thu1 + thu2
+    print("tong hai so nguyen la: ", tong)
+except:
+    print("dinh dang dau vao khong hop le!")
